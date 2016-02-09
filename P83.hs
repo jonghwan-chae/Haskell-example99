3e28a9af5417a77182e3ced2d29758b9abfdf44c
jonghwan-chae <mmi0227@gmail.com>
2016-02-09 19:11:22 +0900

    [Haskell][Example99] add p83, p84 solution

--- P83.hs
+++ P83.hs
@@ -7,20 +7,32 @@ data Graph a = Graph [a] [(a, a)]   deriving (Show, Eq)
 data Adjacency a = Adj [(a, [a])]   deriving (Show, Eq)
 data Friendly a = Edge [(a, a)]     deriving (Show, Eq)
 
+k4 = Graph ['a', 'b', 'c', 'd'] [('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'a'), ('a', 'c'), ('b', 'd')]
+
 {-
 A graph is defined as a set of nodes and a set of edges, where each edge is a pair of nodes.  
 -}
 
-graphToAdj :: Eq a => Graph a -> Adjacency a
-graphToAdj (Graph v e) = Adj (toAdjList v e)
-    where 
-        toAdjList vs es = do
-            v <- vs
-            [(v, toAdj v es)]
-        toAdj v es = do  
-            e <- es
-            if (fst e == v) then [snd e] else if (snd e == v) then [fst e] else []
 
+spantree :: Eq a => Graph a -> [[(a, a)]]
+spantree (Graph v e) = do 
+    item <- filter (len) $ subsequences e
+    if (isTree item) then [item] else []
+    where 
+        n = (length v) - 1
+        len x = (n == length x) 
+
+
+--
+-- Module : check tree
+isTree :: Eq a => [(a, a)] -> Bool
+isTree (e:es) = let vs = [fst e, snd e] in addEdges vs es
+    where addEdges vs edges = case edges of 
+                []     -> True
+                (e:es) -> if (elem (fst e) vs) then if (elem (snd e) vs) then False 
+                                                                         else addEdges (vs ++ [snd e]) es 
+                                               else if (elem (snd e) vs) then addEdges (vs ++ [fst e]) es 
+                                                                         else addEdges vs (es ++ [e])
 
 {-
 Example in Haskell: 
@@ -28,4 +40,6 @@ Example in Haskell:
 length $ spantree k4
 16
 -}
+
+
 
--- P84.hs
+++ P84.hs
@@ -11,16 +11,22 @@ data Friendly a = Edge [(a, a)]     deriving (Show, Eq)
 A graph is defined as a set of nodes and a set of edges, where each edge is a pair of nodes.  
 -}
 
-graphToAdj :: Eq a => Graph a -> Adjacency a
-graphToAdj (Graph v e) = Adj (toAdjList v e)
+prim :: Eq a => [a] -> [(a, a, Int)] -> [(a, a, Int)]
+prim vertexs edges = finds [head vertexs] edges ((length vertexs)-1)
     where 
-        toAdjList vs es = do
-            v <- vs
-            [(v, toAdj v es)]
-        toAdj v es = do  
-            e <- es
-            if (fst e == v) then [snd e] else if (snd e == v) then [fst e] else []
+        finds vertexs edges k 
+            | k == 0    = []
+            | otherwise = let edge = (minx $ checkverts vertexs edges) in edge : finds (addv edge vertexs) edges (k-1)
+        addv (a, b, _) vertexs = if (elem a vertexs) then (vertexs ++ [b]) else (vertexs ++ [a])
 
+        checkverts verts edges = do
+            v <- verts
+            check v verts edges
+        check v1 vs edges = let es = filter (check'' v1 vs) edges in if (0 < length es) then [minx es] else []
+            where check'' v vs (x, y, pri) = if ( (x == v && False == (elem) y vs) || (y == v && False == (elem) x vs) ) then True else False
+        minx list = case list of (a:[]) -> a
+                                 (a:as) -> compare'' a (minx as)
+        compare'' e1@(_, _, p1) e2@(_, _, p2) = if (p1 < p2) then e1 else e2
 
 {-
 Example in Haskell: 
